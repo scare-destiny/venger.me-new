556817a15b99b38807f1117c689b8674fd1b140c
scare-destiny <zhenya.venger@gmail.com>
2023-04-29 14:08:27 +0300

    switched cards to grid

--- components/Testimonials/Testimonials.tsx
+++ components/Testimonials/Testimonials.tsx
@@ -3,7 +3,7 @@ import TestimonialCard from './TestimonialCard'
 
 export default function Testimonials() {
 	return (
-		<div className='pt-16 px-4 flex-col flex gap-4 md:flex-row '>
+		<div className='pt-16 px-4 grid grid-cols-1 gap-4 w-full justify-items-center    '>
 			{testimonials.map((testimonial, index) => (
 				<TestimonialCard key={index} {...testimonial} index={index} />
 			))}
