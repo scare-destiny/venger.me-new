e24c86d9323cfe703f09672f9ad5d3b94f4ec87e
scare-destiny <zhenya.venger@gmail.com>
2023-05-04 20:06:52 +0300

    dark background for dark mode cards

--- components/Testimonials/TestimonialCard.tsx
+++ components/Testimonials/TestimonialCard.tsx
@@ -12,7 +12,7 @@ export default function TestimonialCard({
 	index: number
 }) {
 	return (
-		<div className='flex flex-col-reverse md:flex-row w-full rounded-xl p-10 justify-between relative bg-white shadow-lg max-w-md md:max-w-xl lg:max-w-2xl bg-testimonial-gradient hover:bg-opacity-75 transition-all duration-300 ease-in-out hover:scale-99 hover:shadow-testimonial-hover'>
+		<div className='flex flex-col-reverse md:flex-row w-full rounded-xl p-10 justify-between relative bg-white dark:bg-gray-700 shadow-lg max-w-md md:max-w-xl lg:max-w-2xl bg-testimonial-gradient hover:bg-opacity-75 transition-all duration-300 ease-in-out hover:scale-99 hover:shadow-testimonial-hover'>
 			<div>
 				<p className='mb-2 text-justify'>{content}</p>
 				<h3 className='font-bold '>{name}</h3>
