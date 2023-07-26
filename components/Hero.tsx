6039d48f2ad13860159b1056bdc5d4c57f6f5af6
scare-destiny <zhenya.venger@gmail.com>
2023-07-26 14:42:07 +0300

    updated paddings

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,15 +9,17 @@ export default function Hero() {
 
 	return (
 		<div className={styles.center}>
-			<div className='container flex flex-col justify-start items-center max-w-md md:max-w-7xl pt-8'>
-				<h1 className='typewriter-effect max-w-fit  md:leading-tight lg:leading-snug  font-serif font-bold break-words text-4xl sm:text-6xl lg:text-8xl mb-2'>
+			<div className='container flex flex-col justify-start md:items-center max-w-md md:max-w-7xl pt-8'>
+				<h1 className='typewriter-effect max-w-fit  md:leading-tight lg:leading-snug  font-serif font-bold break-words text-4xl sm:text-6xl lg:text-8xl mb-4 md:mb-2'>
 					<span className={styles.rainbow}>Venger.me</span>
 				</h1>
-				<h1 className='font-serif font-bold break-words text-4xl sm:text-6xl text-center lg:text-8xl mb-2'>
+				<h1 className='font-serif font-bold break-words text-4xl sm:text-6xl md:text-center lg:text-8xl mb-4 md:mb-2'>
 					{title}
 				</h1>
-				<div className='flex flex-col items-start md:pt-2 max-w-md md:max-w-full '>
-					<p className='font-mono text-xl text-center md:text-2xl'>{description}</p>
+				<div className='flex flex-col items-start pt-4 md:pt-2 max-w-md md:max-w-full '>
+					<p className='font-mono text-xl md:text-center md:text-2xl'>
+						{description}
+					</p>
 				</div>
 			</div>
 		</div>
