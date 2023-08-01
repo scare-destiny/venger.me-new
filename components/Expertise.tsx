303c4ed31c7ed4df3fdc2fb4b328341254f80c9a
scare-destiny <zhenya.venger@gmail.com>
2023-08-01 19:14:44 +0300

    expertise update

--- components/Expertise.tsx
+++ components/Expertise.tsx
@@ -1,24 +1,24 @@
 const skills = [
 	{ id: 1, name: 'JavaScript' },
-	{ id: 2, name: 'Node' },
+	{ id: 2, name: 'Node.js' },
 	{ id: 3, name: 'React' },
 	{ id: 4, name: 'Next.js' },
 	{ id: 5, name: 'TypeScript' },
 	{ id: 6, name: 'Postgres' },
 	{ id: 7, name: 'Redux' },
-	{ id: 8, name: 'Next.js' },
+	{ id: 8, name: 'AWS Services' },
 	{ id: 9, name: 'Tailwind CSS' },
 ]
 
 const Expertise = () => {
 	return (
 		<section className='pt-16 max-w-2xl'>
-			<h2 className='font-semibold text-4xl mb-4'>Expertise</h2>
+			<h2 className='font-semibold text-4xl mb-4'>Skills</h2>
 			<p className=' max-w-lg'>
-				I&apos;ve spent few years working on my skills. In no particular order, here
-				are a few of them.
+				Here&apos;s some of the technologies I&apos;ve been working with, listed in
+				no particular order.
 			</p>
-			<ul className='flex flex-wrap items-center gap-3 mt-8'>
+			<ul className='flex flex-wrap items-center gap-3 mt-8 font-mono'>
 				{skills.map((skill) => (
 					<li
 						key={skill.id}
