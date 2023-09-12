f9d90305dc865def9ede262a65ea07446847f2a1
scare-destiny <zhenya.venger@gmail.com>
2023-09-12 18:35:34 +0300

    added whois lib for backend

--- pages/api/checkDomainAge.ts
+++ pages/api/checkDomainAge.ts
@@ -1,27 +1,36 @@
-import { exec } from 'child_process'
+import whois from 'whois'
+
 import type { NextApiRequest, NextApiResponse } from 'next'
+
+interface WhoisResponse {
+	[key: string]: string | number | boolean | null
+}
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  const { domain } = req.body
-  
-  exec(`whois ${domain}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error executing whois: ${error}`)
-      return res.status(500).json({ error: 'Failed to fetch whois data' })
-    }
-    
-    const creationDateLine = stdout.split('\n').find(line => line.includes('Creation Date'))
-    
-    if (!creationDateLine) {
-      return res.status(404).json({ error: 'Creation date not found' })
-    }
-    
-    const creationDate = new Date(creationDateLine.split(':').slice(1).join(':').trim())
-    const now = new Date()
-    const ageInMilliseconds = now.getTime() - creationDate.getTime()
-    const ageInDays = ageInMilliseconds / (1000 * 60 * 60 * 24)
+	const { domain } = req.body
 
-    res.status(200).json({ ageInDays })
-  })
+	whois.lookup(domain, (error: Error | null, data: string) => {
+		if (error) {
+			console.error(`Error executing whois: ${error}`)
+			return res.status(500).json({ error: 'Failed to fetch whois data' })
+		}
+
+		const creationDateLine = data
+			.split('\n')
+			.find((line) => line.includes('Creation Date'))
+
+		if (!creationDateLine) {
+			return res.status(404).json({ error: 'Creation date not found' })
+		}
+
+		const creationDate = new Date(
+			creationDateLine.split(':').slice(1).join(':').trim()
+		)
+		const now = new Date()
+		const ageInMilliseconds = now.getTime() - creationDate.getTime()
+		const ageInDays = ageInMilliseconds / (1000 * 60 * 60 * 24)
+
+		res.status(200).json({ ageInDays })
+	})
 }
